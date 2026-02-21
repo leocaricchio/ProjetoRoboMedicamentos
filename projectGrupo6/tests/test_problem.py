9cc950eefa4304b2160f0f734bbff7066aeb97ad
leocaricchio <leocaricchio@academico.ufs.br>
2026-02-21 00:56:58 -0300

    Refactor tests to use DeliveryProblem instead of HospitalDeliveryProblem

--- projectGrupo6/tests/test_problem.py
+++ projectGrupo6/tests/test_problem.py
@@ -1,17 +1,16 @@
-import pytest
-from problems.hospital_problem import HospitalDeliveryProblem
+from problems.delivery_problem import DeliveryProblem
 from aima3.search import astar_search
 
 
 def test_goal_test():
     state = ((0, 0), frozenset())
-    problem = HospitalDeliveryProblem(state)
+    problem = DeliveryProblem(state, grid_size=5)
     assert problem.goal_test(state) is True
 
 
 def test_result_updates_state():
     state = ((0, 0), frozenset({(1, 0)}))
-    problem = HospitalDeliveryProblem(state)
+    problem = DeliveryProblem(state, grid_size=5)
 
     new_state = problem.result(state, "DOWN")
 
@@ -20,15 +19,22 @@ def test_result_updates_state():
 
 def test_heuristic_non_negative():
     state = ((0, 0), frozenset({(2, 2)}))
-    problem = HospitalDeliveryProblem(state)
+    problem = DeliveryProblem(state, grid_size=5)
 
-    h_value = problem.h(state)
+    # Criar um node simples
+    class Node:
+        def __init__(self, state):
+            self.state = state
+
+    node = Node(state)
+
+    h_value = problem.h(node)
     assert h_value >= 0
 
 
 def test_astar_finds_solution():
     state = ((0, 0), frozenset({(1, 0)}))
-    problem = HospitalDeliveryProblem(state)
+    problem = DeliveryProblem(state, grid_size=5)
 
     solution = astar_search(problem)
 
